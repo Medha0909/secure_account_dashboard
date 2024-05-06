6eee4f347476edc727acd06553d5c843df67d97d
Medha0909 <103240973+Medha0909@users.noreply.github.com>
2024-05-05 19:07:53 -0700

    Delete server directory

--- server/models/db.js
+++
@@ -1,9 +0,0 @@
-//require('dotenv').config();
-const mongoose = require('mongoose');
-
-const connecttomongo=()=>
-{
-    mongoose.connect(process.env.REACT_APP_MONGO_URI);
-}
-
-module.exports=connecttomongo;
