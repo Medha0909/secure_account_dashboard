d2e31481289f68adb4984afbb1da489c962a33dc
Medha0909 <103240973+Medha0909@users.noreply.github.com>
2024-05-05 19:05:26 -0700

    Add files via upload

---
+++ client/pages/account.tsx
@@ -0,0 +1,50 @@
+import React,{useState,useEffect} from 'react';
+import Card from 'react-bootstrap/Card';
+import shield from "H:/Medha_web_development/secure_account_dashboard/client/pages/image/shield.jpg";
+import Image from "next/image";
+
+export default function account() {
+
+  useEffect(()=>{
+    logininfo();
+  },[]);
+
+  const [details,setDetails]=useState([]);
+  const logininfo = async()=>{
+    const res= await fetch("http://localhost:8080/logOutroute/getapi")
+
+      .then((res:any)=>{
+        setDetails(res.data);
+        console.log(details);
+      })
+      
+      .catch((err)=> console.error(err));
+  }
+  
+  return (
+    <div>
+      <Image  className="he1" style={{marginTop:"3rem",marginLeft:"39rem",width:"7rem",height:"7rem"}} src={shield} alt="shield_image" />
+      <h1 className="heading" style={{fontSize:"2rem", marginTop:"2rem",marginLeft:"30rem"}}>Manage Access and Devices</h1>
+      <p style={{marginTop:"1rem",marginLeft:"25rem",marginRight:"25rem"}}>These signed-in devices have recently been ative on this account.You can sign out any unfamiliar devices.</p>
+
+    
+    <div style={{border: "1px solid #999", marginLeft:"20rem",boxShadow:"0 2px 3px #888888", marginTop:"5rem", width:"300px"}}>
+      <div style={{marginTop:"0.5rem",marginLeft:"1rem"}}>
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title><b>Device Information</b>  <button style={{ marginLeft: '3rem',border: "1px solid #999"}}><h4 style={{marginLeft:"5px",marginRight:"5px"}}>Sign Out</h4></button></Card.Title>
+
+        <Card.Text className="mb-2 text-muted">No profile to show</Card.Text>
+        <Card.Text>
+          LogIn time
+        </Card.Text>
+        
+          </Card.Body>
+    </Card>
+    </div>
+    </div>
+    
+  
+    </div>
+  )
+}
---
+++ client/pages/otp.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import Router from 'next/router'  
+import OtpInput from 'react-otp-input';
+
+
+export default function otp(props:any) {
+  const [otp, setOtp] = useState('');
+
+  const [credentials,setCredentials]=useState({email:""})
+      //let navigate=useNavigate();
+    const handleSubmit=async (e:any)=>{
+      e.preventDefault();
+      const email = window.sessionStorage.getItem("email");      
+      const response:any = await fetch("http://localhost:8080/otproutes/verify",{
+      method:'POST',
+        headers:{
+          'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({email, otp})
+      });
+
+      const json = await response.json()
+      
+      console.log({json});
+      if(json.success){
+        //save the authh token and redirect
+        localStorage.setItem('token',json.authtoken);
+        Router.push("/account");
+  
+        //props.showAlert("Account Created Succesfully","success");
+      }
+      else{
+       // props.showAlert("Invalid credentials","danger");
+      }
+    }
+    const onChange=(e:any)=>{
+      setCredentials({...credentials,[e.target.name]: e.target.value})
+    }
+
+
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Enter One Time Password
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" method="POST" onSubmit={handleSubmit}>
+            <div>
+            </div>
+            <div className="a1">
+            <div className="mt-2">
+            <OtpInput
+            value={otp}
+            onChange={setOtp}
+            numInputs={4}
+            renderSeparator={<span>-</span>}
+            renderInput={(props) => <input {...props} />}
+            />
+            </div>
+            </div>
+
+            
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
+
