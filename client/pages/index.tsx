b128c4175abefe4cb6dfe6b5cd076189bb73dbbc
Medha0909 <103240973+Medha0909@users.noreply.github.com>
2024-05-05 20:35:54 -0700

    Update index.tsx

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -9,7 +9,7 @@ function Login(props:any) {
       const handleSubmit=async (e:any)=>{
       e.preventDefault();
       window.sessionStorage.setItem("email",credentials.email);
-      const response = await fetch("http://localhost:8080/reg/login",{
+      const response = await fetch("https://secure-account-dashboard.onrender.com/reg/login",{
         method:'POST',
         headers:{
           'Content-Type': 'application/json'
